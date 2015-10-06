721a7ca2e6d96910837cdc9d866f3328f6a280f0
umberto di fabrizio <umberto.difabrizio@mail.polimi.it>
2015-10-06 02:02:01 -0600

    panel resize and list change piu o meno ok

--- umbos/js/filterView.js
+++ umbos/js/filterView.js
@@ -26,6 +26,7 @@ var filterView = function (){
 	.addTo(this.map);
 
 //****END MOCK-UP
+
 this.filterViewDiv = d3.select('#map').append('div')
 .attr('id','filterView' )
 .attr('class','filterView');
@@ -40,21 +41,144 @@ this.filterViewDiv[0][0].addEventListener('mouseout', function () {
 	filterViewLayout.map.dragging.enable();
 });
 //******///
-
+/*
 this.filterViewDiv
 .on('click',resizeFilterView);
+*/
+//append button to controle resizing of panel
+d3.select('#map')
+.append('g')
+.append('svg')
+.attr('id', 'svg-arrow-filterView')
+.attr('width', 50)
+.attr('height', 50)
+.append('text')
+//.attr('text-anchor', 'middle')
+.attr('dominant-baseline', 'central')
+.attr('font-family', 'FontAwesome')
+.attr('font-size', 10)
+.attr('x', 40 )
+.attr('y', 10)
+	.on('click',resizeFilterView)
+.style('trasform', "translate(30,40)") 
+.text(function(d) { return '\uf139'; });
 
 var toggle = true;
 function resizeFilterView(){
-	var val = (toggle)? 1 : 50;
+	var val = (toggle)? 1 : 25;
 	$('#filterView').animate({'width' : val+'%'}, { duration:750,		
 		complete: function() {
 			toggle = !toggle;
 		}
 	});}
 
+	var svg = this.filterViewDiv
+	.append("svg")
+	.attr("viewBox", "0 0 "+ this.viewBoxWidth+" "+this.viewBoxHeight) //VIEWBOX OF THE filtermap
+	//.attr('preserveAspectRatio','none' )
+	.style('width','100%')
+	.style('height', '100%');
+
+
+	//add title text
+	this.g = svg.append('g');
+	this.g.append('text')
+	.attr('class','title-controls')	
+	.text('CONTROLS')
+	.attr('y', '0')
+	.attr('x', '250');
+
+	//add container svg for lists
+	this.svgList = this.g.append('svg')
+	.attr('id', 'svg-list')	
+	.append('g');
+
+	this.svgList
+	.append('rect')
+	.attr('id', 'rect-list')	
+	.attr('y', '10')
+	.attr('x', '10');
+
+	var dati = ['unomila','duemila','tremila'];
+	var datiNuovi=['cioacioa','giaiagai'];
+
+	//add words
+	this.list = this.svgList
+	.append('g')
+	.selectAll("text")
+	.data(dati)
+	.enter()
+	.append('text')
+	.text(function(d,i){
+		return d;
+	})
+	.attr('y', function(d,i){
+		return i * 100+130;
+	})
+	.attr('x', 30)
+	.attr('color','black')
+	.attr('font-size',20 );
+
+	//add buttons
+	this.svgList
+	.append('text')
+	.attr('text-anchor', 'middle')
+	.attr('dominant-baseline', 'central')
+	.attr('font-family', 'FontAwesome')
+	.attr('font-size', 20)
+	.attr('pointer-events','all' )
+	.attr('x', 770 )
+	.attr('y', 40)
+	.on('click',slideList)
+	.text(function(d) { return '\uf139'; });
+
+	function slideList(){
+console.log('iiooiio');
+		//unbind data
+		filterViewLayout.list = filterViewLayout.list
+		.data([]);
+		
+		//and remove old elements if any
+		filterViewLayout.list
+		.exit()
+		.remove();
+
+		//bind new data so that all the data to add is new
+		filterViewLayout.list = filterViewLayout.list
+		.data(datiNuovi);		
+
+		// add new elements at the end of svg and update the list
+		filterViewLayout.list = filterViewLayout.list
+		.enter()		
+		.append('text')
+		.text(function(d,i){
+			return d;
+		})
+		.attr('y', function(d,i){
+			return 600;
+		})
+		.attr('x', 30)
+		.attr('color','black')
+		.attr('font-size',20 );	
+
+    	// move all the elements to their position
+    	filterViewLayout.list
+    	.transition()
+    	.text(function(d,i){
+    		return d;
+    	})
+    	.attr('y', function(d,i){
+    		return i * 100+130;
+    	})
+    	.attr('x', 30)
+    	.attr('color','black')
+    	.attr('font-size',20 );
+
+    }
+
 
 };
+
 
 filterView();
 
